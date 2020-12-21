566455e8cf6b11e876158b4edb616046e90047c2
Vladimir <woaouh@outlook.com>
2020-12-21 12:41:32 +0200

    Implemented user auth schema. Setup the client

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,90 @@
+const { Router } = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+const config = require('../config/default.json');
+const User = require('../models/User');
+
+const router = Router();
+
+// /api/auth/register
+router.post(
+  '/register',
+  [
+    check('email', 'Incorrect email').isEmail(),
+    check('password', 'Password must be > 6 symbols').isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Wrong credentials when registration',
+        });
+      }
+
+      const { email, password } = req.body;
+      const candidate = await User.findOne({ email });
+
+      if (candidate) {
+        res.status(400).json({ message: 'This email is already exist' });
+        return;
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ email, password: hashedPassword });
+
+      await user.save();
+      res.status(201).json({ message: 'New user has been created' });
+    } catch (e) {
+      res.status(500).json({ message: 'Something went wrong...' });
+    }
+  },
+);
+
+// /api/auth/login
+router.post('/login',
+  [
+    check('email', 'Wrong email').normalizeEmail().isEmail(),
+    check('password', 'Wrong password').exists(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Wrong credentials when login',
+        });
+      }
+
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ message: 'User is not exist' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ message: 'Wrong password' });
+      }
+
+      const token = jwt.sign(
+        { userId: user.id },
+        config.get('jwtSecret'),
+        { expiresIn: '1h' },
+      );
+
+      res.json({ token, userId: user.id });
+    } catch (e) {
+      res.status(500).json({ message: 'Something went wrong...' });
+    }
+  });
+
+module.exports = router;
